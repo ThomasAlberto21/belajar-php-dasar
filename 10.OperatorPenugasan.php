0a50d3b2fb1eb90fb2bfba8f03689c2c00946970
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-20 23:50:43 +0700

    Belajar Operator Penugasan

---
+++ 10.OperatorPenugasan.php
@@ -0,0 +1,21 @@
+<?php
+
+// Todo Operator Penugasan
+// * $a += $b    | $a = $a + b
+// * $a -= $b    | $a = $a - b
+// * $a *= $b    | $a = $a * b
+// * $a /= $b    | $a = $a / b
+// * $a %= $b    | $a = $a % b
+
+
+$total = 0;
+
+$nasi = 10000;
+$buah = 25000;
+$es = 5000;
+
+$total += $nasi;
+$total += $buah;
+$total += $es;
+
+echo "Total semuannya adalah \t : " , $total;
