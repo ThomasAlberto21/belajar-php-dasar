601d2f5250b3951b214f2a64de5879a235827012
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-20 23:42:36 +0700

    Belajar Operasi Aritmatika

---
+++ 9.OperatorAritmatika.php
@@ -0,0 +1,64 @@
+<?php
+
+// Todo Operator Aritmatika
+// * +$variable              | Merubah bilangan menjadi Positif
+// * -$variable              | Merubah bilangan menjadi Negatif
+// * $variable + $variable   | Penjumlahan
+// * $variable - $variable   | Pengurangan
+// * $variable * $variable   | Perkalian
+// * $variable / $variable   | Pembagian
+// * $variable % $variable   | Sisa bagi
+// * $variable ** $variable  | Pangkat
+
+
+
+// Todo 1. +$variable
+$a = 10;
+$b = 10;
+
+$result1 = $a + $b;
+echo "Hasilnya adalah : " , +$result1;
+echo "\n";
+
+// Todo 2. -$variable
+$c = 10;
+$d = 10;
+
+$result2 = $c + $d;
+echo "Hasilnya adalah : " , -$result2;
+echo "\n";
+
+
+// Todo 3. Operasi Penjumlahan
+$penjumlahan = 20 + 10;
+echo "Hasil penjumlahannya adalah \t: " , $penjumlahan;
+echo "\n";
+
+
+// Todo 4. Operasi Pengurangan
+$pengurangan = 20 - 10;
+echo "Hasil pengurangannya adalah \t: " , $pengurangan;
+echo "\n";
+
+
+// Todo 5. Operasi Perkalian
+$perkalian = 20 * 10;
+echo "Hasil perkaliannya adalah \t: " , $perkalian;
+echo "\n";
+
+
+// Todo 6. Operasi Pembagian
+$pembagian= 100 / 10;
+echo "Hasil pembagiannya adalah \t: " , $pembagian;
+echo "\n";
+
+
+// Todo 7. Operasi sisa bagi
+$sisa_bagi= 100 % 10;
+echo "Hasil sisa baginya  adalah \t: " , $sisa_bagi;
+echo "\n";
+
+// Todo 8. Operasi pangkat
+$pangkat= 2**2;
+echo "Hasil pangkatnya  adalah \t: " , $pangkat;
+echo "\n";
