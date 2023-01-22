f493eb20ff59c8e610a70faf6fa78439508cc6d7
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-22 12:20:25 +0700

    Belajar While Loop

---
+++ 22.WhileLoop.php
@@ -0,0 +1,13 @@
+<?php
+
+// Todo While Loop
+// * While loop adalah versi perulangan yang lebih sederhana dibanding for loop
+// * Di while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement 
+
+
+$while_loop = 1;
+
+while($while_loop <= 10){
+    echo "Ini adalah perulangan while loop ke-$while_loop" . PHP_EOL;
+    $while_loop++;
+}
