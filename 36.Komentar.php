8aa1a40f82dfb64ad5be1d9425d869e27410bc29
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-23 20:25:16 +0700

    Belajar Komentar

---
+++ 36.Komentar.php
@@ -0,0 +1,13 @@
+<?php
+
+// Todo Komentar
+// * Kadang dalam membuat program, kita sering menempatkan komentar di kode program tersebut
+// * Komentar adalah kode program yang akan di hiraukan saat kode program kita dijalankan
+// * Biasanya komentar digunakan untuk dokumentasi
+
+
+// Todo ini adalah komentar multi baris
+/*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis temporibus libero doloremque minus dolorem, nobis quidem obcaecati cumque iusto voluptates autem facilis neque quo ex explicabo alias culpa incidunt enim fuga nostrum tenetur aperiam perferendis? Perferendis sunt quis molestias, mollitia eius quos. Repellendus voluptates esse similique harum totam suscipit modi itaque numquam quo, quaerat libero temporibus reprehenderit cum quis magnam quisquam dolore quae voluptas laboriosam aliquid dolores voluptatum reiciendis. Inventore fugiat voluptas vel, hic et, assumenda labore unde nam odit delectus nemo deleniti reprehenderit pariatur consequatur ipsam, a vero facere ex eaque ratione quos culpa? Recusandae voluptatum ducimus qui, sunt sit nostrum dignissimos necessitatibus hic nihil eligendi quisquam incidunt molestias autem similique ut iusto! Placeat vitae veritatis maiores reprehenderit repellendus quis, rerum deserunt magni vel cupiditate eum aliquam tempore earum quibusdam, quidem exercitationem esse iure atque aut error maxime ab nemo. Minus ipsa numquam aspernatur, molestiae repudiandae iure impedit provident nisi sit alias dolore aperiam suscipit quidem dolor illo pariatur ex corporis voluptatum amet unde quod magnam voluptatem? Rerum, debitis, id hic quos aliquid ea voluptate exercitationem corporis animi consequatur nihil? Corporis reiciendis laboriosam officiis quo officia, enim deleniti minima hic aperiam aliquam cupiditate inventore vero dolore? Dignissimos, excepturi dolore?
+*/
+
+// Todo ini adalah komentar satu baris
